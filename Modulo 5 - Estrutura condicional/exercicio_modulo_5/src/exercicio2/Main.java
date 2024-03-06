104b99f2eb8c5642e4aac7b07ff8389d327d8958
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-03-06 18:44:31 -0300

    Adicionando resolução do exercicio2

--- Modulo 5 - Estrutura condicional/exercicio_modulo_5/src/exercicio2/Main.java
+++ Modulo 5 - Estrutura condicional/exercicio_modulo_5/src/exercicio2/Main.java
@@ -1,10 +1,26 @@
 package exercicio2;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner entrada = new Scanner(System.in);
 
+        int nmrEntrada = entrada.nextInt();
+
+        if ((nmrEntrada * nmrEntrada) % 2 == 0){
+
+            System.out.println("PAR");
+
+        } else if ((nmrEntrada * nmrEntrada) % 2 == 1) {
+
+            System.out.println("IMPAR");
+
+        }
+
+        entrada.close();
 
     }
 
