a84ecc86136acadfd0868be46a9030280a901f80
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-03-06 18:30:03 -0300

    Adicionando resolução do exercício1

--- Modulo 5 - Estrutura condicional/exercicio_modulo_5/src/exercicio1/Main.java
+++ Modulo 5 - Estrutura condicional/exercicio_modulo_5/src/exercicio1/Main.java
@@ -1,4 +1,27 @@
 package exercicio1;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        int nmrEntrada = entrada.nextInt();
+
+        if (nmrEntrada < 0){
+
+            System.out.println("NEGATIVO");
+
+        }else if (nmrEntrada > 0) {
+
+            System.out.println("NAO NEGATIVO");
+
+        }
+
+        entrada.close();
+
+    }
+
 }
