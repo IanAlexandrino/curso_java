ee07534b5d342d8b2e5ecf5c91111f3685f2082a
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-03-07 10:38:29 -0300

    Implementando classe Employee e seus métodos

--- Modulo 8 - Introdução à Programação Orientada a Objetos/exercicio_fixacao/src/exercicio2/entidades/Employee.java
+++ Modulo 8 - Introdução à Programação Orientada a Objetos/exercicio_fixacao/src/exercicio2/entidades/Employee.java
@@ -1,4 +1,52 @@
 package exercicio2.entidades;
 
 public class Employee {
+
+    private String name;
+    private double grossSalary;
+    private double tax;
+
+    public Employee() {
+    }
+
+    public Employee(String name, double grossSalary, double tax) {
+        this.name = name;
+        this.grossSalary = grossSalary;
+        this.tax = tax;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getGrossSalary() {
+        return grossSalary;
+    }
+
+    public void setGrossSalary(double grossSalary) {
+        this.grossSalary = grossSalary;
+    }
+
+    public double getTax() {
+        return tax;
+    }
+
+    public void setTax(double tax) {
+        this.tax = tax;
+    }
+
+    public String netSalary(){
+        return "Employee: " + name + ", $ " + (grossSalary - tax);
+    }
+
+    public void increaseSalary(double percentage){
+
+        double newIncrease = (percentage * grossSalary) / 100;
+
+        System.out.println("Update data: " + name + ", $ " + (grossSalary - tax) + newIncrease);
+    }
 }
