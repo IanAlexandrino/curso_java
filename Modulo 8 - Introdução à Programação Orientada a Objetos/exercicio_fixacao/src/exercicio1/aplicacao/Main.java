f0327cde4a323164953d1536bed4fa7e828022a2
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-03-07 10:22:58 -0300

    CImplementando camanoaods métodos da classe Rectangle na main

--- Modulo 8 - Introdução à Programação Orientada a Objetos/exercicio_fixacao/src/exercicio1/aplicacao/Main.java
+++ Modulo 8 - Introdução à Programação Orientada a Objetos/exercicio_fixacao/src/exercicio1/aplicacao/Main.java
@@ -1,7 +1,26 @@
 package exercicio1.aplicacao;
 
+import exercicio1.entidades.Rectangle;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter rectangle widht and height: ");
+
+        double auxWidht = sc.nextDouble();
+        double auxHeight = sc.nextDouble();
+
+        Rectangle rectangle = new Rectangle(auxWidht, auxHeight);
+
+        System.out.printf("AREA = %.2f%n", rectangle.area());
+        System.out.printf("PERIMETER = %.2f%n", rectangle.perimeter());
+        System.out.printf("DIAGONAL = %.2f%n", rectangle.diagonal());
+
     }
 }
