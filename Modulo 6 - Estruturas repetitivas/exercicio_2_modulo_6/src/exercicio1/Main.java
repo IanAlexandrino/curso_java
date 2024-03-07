1a1723b96b55a22c2c516ae705d5ba1a00f87280
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-03-07 07:08:08 -0300

    Adicionando pacote com exercício 1 completo

---
+++ Modulo 6 - Estruturas repetitivas/exercicio_2_modulo_6/src/exercicio1/Main.java
@@ -0,0 +1,29 @@
+package exercicio1;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int valor = sc.nextInt();
+
+        while (valor < 1 || valor > 1000){
+
+            valor = sc.nextInt();
+
+        }
+
+        for (int i = 1 ; i <= valor ; i++){
+
+            if (i % 2 != 0){
+
+                System.out.println(i);
+
+            }
+
+        }
+    }
+
+}
