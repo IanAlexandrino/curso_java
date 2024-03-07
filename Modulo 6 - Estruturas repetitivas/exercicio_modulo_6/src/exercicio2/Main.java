e37b36a780b47df806683ad6562caf938d6a287e
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-03-07 05:15:39 -0300

    Adicionando pacote com exercício 2 completo

---
+++ Modulo 6 - Estruturas repetitivas/exercicio_modulo_6/src/exercicio2/Main.java
@@ -0,0 +1,42 @@
+package exercicio2;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+        int x = entrada.nextInt();
+        int y = entrada.nextInt();
+
+        while (x != 0 && y != 0){
+
+            if (x > 0 && y > 0){
+
+                System.out.println("primeiro");
+
+            } else if (x < 0 && y > 0) {
+
+                System.out.println("segundo");
+
+            } else if (x < 0 && y < 0) {
+
+                System.out.println("terceiro");
+
+            } else if (x > 0 && y < 0) {
+
+                System.out.println("quarto");
+
+            }
+
+            x = entrada.nextInt();
+            y = entrada.nextInt();
+
+        }
+
+        entrada.close();
+
+    }
+
+}
