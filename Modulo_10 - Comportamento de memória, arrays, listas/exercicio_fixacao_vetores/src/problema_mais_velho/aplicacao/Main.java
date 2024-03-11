6f4f8a3cb824be3f09e4ba607dc737ad541087cf
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-03-11 18:55:46 -0300

    Adicionando os arquivos base para resolução do problema mais_velho e implementando classe Pessoa

---
+++ Modulo_10 - Comportamento de memória, arrays, listas/exercicio_fixacao_vetores/src/problema_mais_velho/aplicacao/Main.java
@@ -0,0 +1,15 @@
+package problema_mais_velho.aplicacao;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        sc.close();
+
+    }
+
+}
