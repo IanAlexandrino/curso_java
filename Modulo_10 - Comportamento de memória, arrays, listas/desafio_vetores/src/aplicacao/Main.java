b6e0b1fb6f6e0ed5234c11c684ed7123919da989
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-03-11 20:05:30 -0300

    Adicionando arquivos base do projeto que vai ser utilizado para resolver desafio sobre vetores

---
+++ Modulo_10 - Comportamento de memória, arrays, listas/desafio_vetores/src/aplicacao/Main.java
@@ -0,0 +1,15 @@
+package aplicacao;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        sc.close();
+
+    }
+
+}
