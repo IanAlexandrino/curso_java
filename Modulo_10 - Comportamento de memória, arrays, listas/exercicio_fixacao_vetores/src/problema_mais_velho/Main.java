4cac6d8429c4e1c0a17dc92d05b1baa8ecacfdc5
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-03-11 19:09:07 -0300

    Implementando resolução do problema mais_velho

--- Modulo_10 - Comportamento de memória, arrays, listas/exercicio_fixacao_vetores/src/problema_mais_velho/Main.java
+++ Modulo_10 - Comportamento de memória, arrays, listas/exercicio_fixacao_vetores/src/problema_mais_velho/Main.java
@@ -8,6 +8,44 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("Quantas pessoas voce vai digitar? ");
+        int tamanhoVectors = sc.nextInt();
+        int idadeMaisVelho = 0;
+
+        String[] vectorNome = new String[tamanhoVectors];
+        int[] vectorIdade = new int[tamanhoVectors];
+
+        for (int i = 0 ; i < tamanhoVectors ; i++){
+
+            sc.nextLine();
+            System.out.println("Dados da " + (i + 1) + "ª pessoa");
+            System.out.print("Nome: ");
+            vectorNome[i] = sc.nextLine();
+            System.out.print("Idade: ");
+            vectorIdade[i] = sc.nextInt();
+
+        }
+
+        for (int i = 0 ; i < tamanhoVectors ; i++){
+
+            if (vectorIdade[i] > idadeMaisVelho){
+
+                idadeMaisVelho = vectorIdade[i];
+
+            }
+
+        }
+
+        for (int i = 0 ; i < tamanhoVectors ; i++){
+
+            if (vectorIdade[i] == idadeMaisVelho){
+
+                System.out.println("PESSOA MAIS VELHA : " + vectorNome[i]);
+
+            }
+
+        }
+
         sc.close();
 
     }
