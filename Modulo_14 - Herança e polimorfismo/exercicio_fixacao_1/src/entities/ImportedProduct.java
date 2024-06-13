3f96e1641f88147ab59ccc327b08ab8bd4b3a511
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-06-13 13:10:18 -0300

    feat: adding relationship between entities and your methods

--- Modulo_14 - Herança e polimorfismo/exercicio_fixacao_1/src/entities/ImportedProduct.java
+++ Modulo_14 - Herança e polimorfismo/exercicio_fixacao_1/src/entities/ImportedProduct.java
@@ -1,4 +1,35 @@
 package entities;
 
-public class ImportedProduct {
+public final class ImportedProduct extends Product{
+
+    private double customsFee;
+
+    public ImportedProduct() {
+        super();
+    }
+
+    public ImportedProduct(String name, double price, double customsFee) {
+        super(name, price);
+        this.customsFee = customsFee;
+    }
+
+    public double getCustomsFee() {
+        return customsFee;
+    }
+
+    public void setCustomsFee(double customsFee) {
+        this.customsFee = customsFee;
+    }
+
+    @Override
+    public String priceTag(){
+        return super.priceTag()
+                + " (Customs fee: $ "
+                + String.format("%.2f", customsFee)
+                + ")";
+    }
+
+    public double totalPrice(){
+        return getPrice() + customsFee;
+    }
 }
