8a568ec9b9575dde8bdb7a8b4e5668a532a234f7
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-06-14 16:48:29 -0300

    feat: adding relationship between entities and your methods

--- Modulo_14 - Herança e polimorfismo/exercicio_fixacao_2/src/entities/LegalPerson.java
+++ Modulo_14 - Herança e polimorfismo/exercicio_fixacao_2/src/entities/LegalPerson.java
@@ -1,4 +1,32 @@
 package entities;
 
-public class LegalPerson {
+public final class LegalPerson extends Person{
+
+    private Integer numberOfEmployees;
+
+    public LegalPerson() {
+        super();
+    }
+
+    public LegalPerson(String name, Double annualIncome, Integer numberOfEmployees) {
+        super(name, annualIncome);
+        this.numberOfEmployees = numberOfEmployees;
+    }
+
+    public Integer getNumberOfEmployees() {
+        return numberOfEmployees;
+    }
+
+    public void setNumberOfEmployees(Integer numberOfEmployees) {
+        this.numberOfEmployees = numberOfEmployees;
+    }
+
+    @Override
+    public double tax(){
+        if (numberOfEmployees > 10){
+            return ((14.0 / 100.0) * annualIncome);
+        } else {
+            return ((16.0 / 100.0) * annualIncome);
+        }
+    }
 }
--- Modulo_14 - Herança e polimorfismo/exercicio_fixacao_2/src/entities/PhysicalPerson.java
+++ Modulo_14 - Herança e polimorfismo/exercicio_fixacao_2/src/entities/PhysicalPerson.java
@@ -1,4 +1,40 @@
 package entities;
 
-public class PhysicalPerson {
+public final class PhysicalPerson extends Person{
+
+    private Double healthExpenditures;
+
+    public PhysicalPerson() {
+        super();
+    }
+
+    public PhysicalPerson(String name, Double annualIncome, Double healthExpenditures) {
+        super(name, annualIncome);
+        this.healthExpenditures = healthExpenditures;
+    }
+
+    public Double getHealthExpenditures() {
+        return healthExpenditures;
+    }
+
+    public void setHealthExpenditures(Double healthExpenditures) {
+        this.healthExpenditures = healthExpenditures;
+    }
+
+    @Override
+    public double tax(){
+        if (annualIncome >= 20000){
+            if (healthExpenditures > 0){
+                return ((25.0 / 100.0) * annualIncome) - ((50.0 / 100.0) * healthExpenditures);
+            } else {
+                return ((25.0 / 100.0) * annualIncome);
+            }
+        } else {
+            if (healthExpenditures > 0){
+                return ((15.0 / 100.0) * annualIncome) - ((50.0 / 100.0) * healthExpenditures);
+            } else {
+                return ((15.0 / 100.0) * annualIncome);
+            }
+        }
+    }
 }
