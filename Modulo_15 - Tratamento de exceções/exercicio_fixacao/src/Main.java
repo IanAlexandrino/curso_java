dd7d591d9979e9b95848f3296c114f1f15da0a17
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-06-15 15:51:07 -0300

    feat: adding main to the program

--- Modulo_15 - Tratamento de exceções/exercicio_fixacao/src/Main.java
+++ Modulo_15 - Tratamento de exceções/exercicio_fixacao/src/Main.java
@@ -1,5 +1,43 @@
+import model.entities.Account;
+import model.exceptions.AccountException;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter account data ");
+        System.out.print("Number: ");
+        int number = sc.nextInt();
+
+        System.out.print("Holder: ");
+        sc.nextLine();
+        String holder = sc.nextLine();
+
+        System.out.print("Initial balance: ");
+        double balance = sc.nextDouble();
+
+        System.out.print("Withdraw limit: ");
+        double withdrawLimit = sc.nextDouble();
+
+        Account account = new Account(number, holder, balance, withdrawLimit);
+
+        System.out.print("\nEnter amount for withdraw: ");
+        double amount = sc.nextDouble();
+
+        try {
+            account.withdraw(amount);
+            System.out.printf("New balance: %.2f", account.getBalance());
+        } catch (AccountException e) {
+            System.out.println("Withdraw error: " + e.getMessage());
+        } catch (RuntimeException e) {
+            System.out.println("Unexpected error: " + e.getMessage());
+        }
+
+        sc.close();
     }
 }
