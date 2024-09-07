6b11af75d707fe29282bff675ee725e8c7d428cf
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-09-06 23:32:37 -0300

    feat: adding the product structures

--- Modulo_17 - Trabalhando com arquivos/exercicio_proposto/src/entities/Product.java
+++ Modulo_17 - Trabalhando com arquivos/exercicio_proposto/src/entities/Product.java
@@ -1,4 +1,51 @@
 package entities;
 
 public class Product {
+
+    private String name;
+    private double value;
+    private int quantity;
+    private double totalValue;
+
+    public Product() {
+    }
+
+    public Product(String name, double value, int quantity) {
+        this.name = name;
+        this.value = value;
+        this.quantity = quantity;
+        this.totalValue = value * quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getTotalValue() {
+        return totalValue;
+    }
+
+    public void setTotalValue(double totalValue) {
+        this.totalValue = totalValue;
+    }
 }
