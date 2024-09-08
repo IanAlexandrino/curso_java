36a03b2a855a2a3d3e6ca3537bfa9bbbdf39ea8f
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-09-07 22:40:51 -0300

    feat: adding the exercise resolution

---
+++ Modulo_17 - Trabalhando com arquivos/exercicio_proposto/README.md
@@ -0,0 +1,3 @@
+# IMPORTANTE
+
+A atividade pede para criar uma pasta out e criar o arquivo summary.csv para passar os dados de data.csv, no entanto, o intellij que é a IDE que estou utilizando tem a pasta out como arquivo para build, então estou mudando o nome da pasta para output mas a ideia e a lógica para o exercício continua a mesma!!
--- Modulo_17 - Trabalhando com arquivos/exercicio_proposto/src/application/Main.java
+++ Modulo_17 - Trabalhando com arquivos/exercicio_proposto/src/application/Main.java
@@ -1,7 +1,59 @@
 package application;
 
+import entities.Product;
+
+import java.io.*;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String pathSource = "c:\\Users\\Ian Rodrigues\\Documents\\PROJETOS\\Curso JAVA COMPLETO\\Modulo_17 - Trabalhando com arquivos\\exercicio_proposto\\data.csv";
+        String pathOut = "c:\\Users\\Ian Rodrigues\\Documents\\PROJETOS\\Curso JAVA COMPLETO\\Modulo_17 - Trabalhando com arquivos\\exercicio_proposto\\output";
+        File path = new File(pathOut);
+        ArrayList<Product> products = new ArrayList<>();
+        boolean folderCreationCondition = true;
+
+        if (!path.canExecute()){
+            folderCreationCondition = new File(pathOut).mkdir();
+        }
+
+        if (folderCreationCondition){
+
+            try (BufferedReader br = new BufferedReader(new FileReader(pathSource))){
+
+                String line = br.readLine();
+
+                while (line != null){
+
+                    String[] productFeatures = line.split(",");
+                    Product product = new Product(productFeatures[0], Double.parseDouble(productFeatures[1]), Integer.parseInt(productFeatures[2]));
+                    products.add(product);
+                    line = br.readLine();
+
+                }
+
+            } catch (IOException e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter(path + "\\summary.csv"))){
+
+                for (Product product : products){
+
+                    bw.write(product.getName() + ",");
+                    bw.write(String.format("%.2f", product.getTotalValue()));
+                    bw.newLine();
+
+                }
+
+            } catch (IOException e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+
+        } else {
+            System.out.println("Não foi possível criar a pasta de destino!");
+        }
+
     }
 }
